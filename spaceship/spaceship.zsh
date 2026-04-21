33130affa5ce7efbe5ef51fba584c273b6ca9bfd
Pedro Simões <pedroalsim@gmail.com>
2026-04-21 02:56:21 +0100

    Changed spaceship prompt and simplified zshrc

--- spaceship/spaceship.zsh
+++ spaceship/spaceship.zsh
@@ -1,16 +1,12 @@
-# Display time
-SPACESHIP_TIME_SHOW=true
-
-# Display username always
-SPACESHIP_USER_SHOW=always
 
 # Do not truncate path in repos
-SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_DIR_TRUNC_REPO=2
 
-# Add custom Ember section
-# See: https://github.com/spaceship-prompt/spaceship-ember
-spaceship add ember
+# Remove stuff
+spaceship remove package
 
-# Add a custom vi-mode section to the prompt
-# See: https://github.com/spaceship-prompt/spaceship-vi-mode
-spaceship add --before char vi_mode
+# Prompt
+SPACESHIP_CHAR_SYMBOL="ƛ "
+SPACESHIP_DIR_BOLD=true
+SPACESHIP_CHAR_COLOR_SUCCESS="130"
+SPACESHIP_CHAR_COLOR_FAILURE="130"
